b6db8d94b49ae107778978c0e876f2f4f6aa826a
Hasankhankor <hkhankor@gmail.com>
2025-04-30 00:08:44 +0500

    done

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'DesignMySuit.pk',
+  description: ' Create custom Pakistani suits with our interactive designer. Choose fabrics ',
+  generator: 'Hasan',
 }
 
 export default function RootLayout({
